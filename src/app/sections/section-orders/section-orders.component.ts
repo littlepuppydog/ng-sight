29579ce25af9721e608bdd91ccbfdb95307472e3
Thomas Liu <thomasl@snell.co.nz>
2019-03-21 00:02:48 +1300

    Add sample data for orders page

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -11,7 +11,41 @@ export class SectionOrdersComponent implements OnInit {
   constructor() { }
 
   orders : Order[] = [
-    {}
+    {
+      id: 1,
+      customer: {id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},
+      total: 230, 
+      placed: new Date(2017,12,13),  
+      fulfilled: new Date(2017,12,14),     
+    },
+    {
+      id: 2,
+      customer: {id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},
+      total: 230, 
+      placed: new Date(2017,12,1),  
+      fulfilled: new Date(2017,12,3),     
+    },
+    {
+      id: 3,
+      customer: {id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},
+      total: 230, 
+      placed: new Date(2017,12,1),  
+      fulfilled: new Date(2017,12,3),     
+    },
+    {
+      id: 4,
+      customer: {id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},
+      total: 230, 
+      placed: new Date(2017,12,1),  
+      fulfilled: new Date(2017,12,3),     
+    },
+    {
+      id: 5,
+      customer: {id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},
+      total: 230, 
+      placed: new Date(2017,12,1),  
+      fulfilled: new Date(2017,12,3),     
+    }
   ]
 
   ngOnInit() {
