e729e19014dad737dfb733f929b3f97b53565817
Thomas Liu <thomasl@snell.co.nz>
2019-03-21 17:40:31 +1300

    Update server button

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -10,7 +10,30 @@ export class ServerComponent implements OnInit {
 
   constructor() { }
 
+  color: string;
+  buttonText: string;
+
+  @Input() serverInput: Server;
+
   ngOnInit() {
+
+  }
+
+  setServerStatus(isOnline: boolean) {
+    if (isOnline) {
+      this.serverInput.isOnline = true;
+      this.color = "#66bb6a";
+      this.buttonText = "Shut Down";
+    } else {
+      this.serverInput.isOnline = false;
+      this.color = "#ff6b6b";
+      this.buttonText = "Start";
+    }
+  }
+
+  toggleStatus(onlineStatus: boolean) {
+    console.log(this.serverInput.name, ': ', onlineStatus);
+    this.setServerStatus(!onlineStatus);
   }
 
 }
