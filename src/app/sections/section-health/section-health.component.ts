4d0c8f96eebf0bbbcf70ad289ad3d5285c827087
Thomas Liu <thomasl@snell.co.nz>
2019-03-25 10:55:30 +1300

    Example

--- src/app/sections/section-health/section-health.component.ts
+++ src/app/sections/section-health/section-health.component.ts
@@ -3,7 +3,7 @@ import { Server } from '../../shared/server';
 
 const SAMPLE_SERVERS = [
   {id: 1, name: 'dev-web', isOnline: true},
-  {id: 2, name: 'dev-mail', isOnline: false},
+  {id: 2, name: 'dev-mail', isOnline: true},
   {id: 3, name: 'prod-web', isOnline: true},
   {id: 4, name: 'prod-mail', isOnline: true}
 ];
